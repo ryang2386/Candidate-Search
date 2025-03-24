3dddc159ba096e2158de51e984bf9ec96748da4d
Ryan Gayle <gayler02@nyu.edu>
2025-03-23 20:59:09 -0400

    updated Candidate interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,10 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+interface Candidate {
+    name: string;
+    username: string;
+    location: string;
+    avatar_url: string;
+    email: string;
+    html_url: string;
+    company: string;
+}
