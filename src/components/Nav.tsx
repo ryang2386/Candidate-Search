71b167b9338dd04ba770e24cdf08eef8790cb889
Ryan Gayle <gayler02@nyu.edu>
2025-03-23 20:25:24 -0400

    updated Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,25 @@
+import { Link, useLocation } from 'react-router-dom';
+
 const Nav = () => {
+
+  const location = useLocation().pathname;
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/" className={location === '/' ? 'active' : ''}></Link>
+          </li>
+          <li>
+            <Link to="/candidatesearch" className={location === '/candidatesearch' ? 'active' : ''}>Candidate Search</Link>
+          </li>
+          <li>
+            <Link to="/potentialcandidates" className={location === '/potentialcandidates' ? 'active' : ''}>Potential Candidates</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 };
 
