b4361635cb76e2d0cef64aeb3a6392a045cdc09e
Ryan Gayle <gayler02@nyu.edu>
2025-03-30 20:11:11 -0400

    updated name type to string | null

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,7 +1,7 @@
 // TODO: Create an interface for the Candidate objects returned by the API
 interface Candidate {
     login: string;
-    name: string;
+    name: string | null;
     location: string | null;
     avatar_url: string;
     email: string | null;
