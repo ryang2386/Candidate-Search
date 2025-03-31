cc61779c9dbf47789c020eccd0c6a87cf231d7fa
Ryan Gayle <gayler02@nyu.edu>
2025-03-30 20:09:58 -0400

    fixed logic for info shown, created remove button

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, FormEvent } from 'react';
 import { searchGithubUser } from '../api/API';
+import { useNavigate } from 'react-router-dom';
 
 const CandidateSearch = () => {
 
@@ -8,27 +9,46 @@ const CandidateSearch = () => {
   const [searchInput, setSearchInput] = useState<string>('');
   const [noCandidatesMessage, setNoCandidatesMessage] = useState<string>('');
 
+  const navigate = useNavigate();
+
   const addToCandidatesList = () => {
     const storedCandidates = localStorage.getItem('candidates');
     let candidatesList: Candidate[] = storedCandidates ? JSON.parse(storedCandidates) : [];
-    if (candidatesList.length === 0) {
-      setNoCandidatesMessage('No more candidates are available.');
-    } else{
-      setNoCandidatesMessage('');
-    }
 
     if (currentCandidate.login) {  
-      console.log('currentCandidate', currentCandidate);  
+      // console.log('currentCandidate', currentCandidate);  
       candidatesList.push(currentCandidate);
     localStorage.setItem('candidates', JSON.stringify(candidatesList));
+    setNoCandidatesMessage('Candidate added to the list. Please search for another.');
     }
   };
+
+const removeFromCandidatesList = () => {
+  const storedCandidates = localStorage.getItem('candidates');
+    let candidatesList: Candidate[] = storedCandidates ? JSON.parse(storedCandidates) : [];
+  
+  
+  if (currentCandidate.login) {  
+    // if last candidate is removed from local storage, default back to home page
+    candidatesList.shift();
+    // console.log('currentCandidate', currentCandidate);
+    if (candidatesList.length === 0) {
+      localStorage.removeItem('candidates');
+      setCurrentCandidate({ login: '', name: '', location: '', avatar_url: '', email: '', html_url: '', company: '' });
+      navigate('/');
+    } else {
+      localStorage.setItem('candidates', JSON.stringify(candidatesList));
+      setCurrentCandidate(candidatesList[0]);
+    }
+    }
+};
 
 const searchForCandidates = async (event: FormEvent, username: string) => {
   event.preventDefault();
   try {
   const candidate: Candidate = await searchGithubUser(username);
-  if (candidate) {
+  if (candidate && candidate.login) {
+    console.log('Candidate:', candidate);
     setCurrentCandidate(candidate);
     setNoCandidatesMessage('');
   } else {
@@ -50,30 +70,31 @@ useEffect(() => {
 }, []);
 
 return (
-<>
-  <div>
-    <h1>Candidate Search</h1>
-    <form onSubmit={(event: FormEvent) => searchForCandidates(event, searchInput)}>
-      <input type='text' name='' placeholder='Enter Candidate Username' onChange={(event) => setSearchInput(event.target.value)} />
-      <button type='submit'>Search</button>
-    </form>
-  </div>
-  {noCandidatesMessage ? (<p>{noCandidatesMessage}</p>) : (
-  <div>
-    <section className="candidateCard">
-      <img src={currentCandidate.avatar_url} alt='avatar' className='img' />
-      <h2>{currentCandidate.login}</h2>
-      <p>{currentCandidate.name}</p>
-      <p>{currentCandidate.location}</p>
-      <p>{currentCandidate.email}</p>
-      <p>{currentCandidate.html_url}</p>
-      <p>{currentCandidate.company}</p>
-    </section>
-    <button onClick={addToCandidatesList}>Add to Candidates List</button>
-  </div>
-  )}
-  </>
-  );
-};
+    <>
+      <div>
+        <h1>Candidate Search</h1>
+        <form onSubmit={(event: FormEvent) => searchForCandidates(event, searchInput)}>
+          <input type='text' name='' placeholder='Enter Candidate Username' onChange={(event) => setSearchInput(event.target.value)} />
+          <button type='submit'>Search</button>
+        </form>
+      </div>
+      {noCandidatesMessage ? (<p>{noCandidatesMessage}</p>) : currentCandidate.login ? (
+      <div>
+        <section className="candidateCard">
+          <img src={currentCandidate.avatar_url} alt='avatar' className='img' />
+          <h2>{currentCandidate.login}</h2>
+          {currentCandidate.name ? (<p>Name: {currentCandidate.name}</p>) : null}
+          {currentCandidate.location ? (<p>Location: {currentCandidate.name}</p>) : null}
+          {currentCandidate.email ? (<p>Email: {currentCandidate.email}</p>) : null}
+          <p>URL: {currentCandidate.html_url}</p>
+          {currentCandidate.company ? (<p>Company: {currentCandidate.company}</p>) : null}
+        </section>
+        <button onClick={addToCandidatesList}>Add</button>
+        <button onClick={removeFromCandidatesList}>Remove</button>
+      </div>
+      ) : null }
+      </>
+      );
+    };
 
 export default CandidateSearch;
